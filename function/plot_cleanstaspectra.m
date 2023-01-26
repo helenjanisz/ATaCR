7e1024a136b8043ee7b92527dcab7215f15839c6
SeisPiano <12131066@mail.sustech.edu.cn>
2023-01-26 17:21:01 +0800

    added the user choice of component options

--- function/plot_cleanstaspectra.m
+++ function/plot_cleanstaspectra.m
@@ -1,7 +1,16 @@
-function plot_cleanstaspectra(spect,coh_stack,ph_stack,ad_stack,cc,station,f,maxpow,minpow);
+function plot_cleanstaspectra(spect,coh_stack,ph_stack,ad_stack,cc,station,f,maxpow,minpow,comporder,plotorder)
 
-comporder = {'Z','H1','H2','P'};
-plotorder = {'1Z','2Z','HZ','PZ','12','P1','P2'};
+all_comporder = {'Z','H1','H2','P'};
+all_plotorder = {'1Z','2Z','HZ','PZ','12','P1','P2'};
+
+for ico = 1:length(comporder)
+    co_id(ico) = find(ismember(all_comporder,comporder(ico)));
+end
+
+for ipo = 1:length(plotorder)
+    po_id(ipo) = find(ismember(all_plotorder,plotorder(ipo)));
+end
+
 
 % Plotting Power Spectra
 figure(1)
@@ -9,22 +18,23 @@ set(gcf,'PaperPositionMode','manual');
 set(gcf,'PaperUnits','inches');
 set(gcf,'PaperOrientation','portrait');
 set(gcf,'PaperPosition',[.05 .05 8 10.5]);
+
 for ip = 1:length(comporder)
     subplot(length(comporder),1,ip)
     set(gca,'ColorOrder',cc,'NextPlot','replacechildren');
-    loglog(f,spect(:,:,ip),'-','LineWidth',.5)%,'Color',cc')
+    loglog(f,spect(:,:,co_id(ip)),'-','LineWidth',.5);
 %     hold on
     title(sprintf('%s-component, Station: %s',comporder{ip},station));
     xlabel('Frequency (Hz)')
-    ylabel('Power (db)')
+    ylabel('Power (dB)')
     set(gca,'yscale','log','xscale','log');
-    xlim([10^-4 max(f)]); 
-    if isnan(minpow(ip))
-        ylim([ 0 1])
-    elseif minpow(ip)==maxpow(ip)
-        ylim([ 0 1])
+    xlim([10e-4 max(f)]); 
+    if isnan(minpow(co_id(ip)))
+        ylim([0 1])
+    elseif minpow(co_id(ip))==maxpow(co_id(ip))
+        ylim([0 1])
     else
-    ylim([minpow(ip) maxpow(ip)]);
+        ylim([minpow(co_id(ip)) maxpow(co_id(ip))]);
     end
     box on
 end
@@ -51,7 +61,7 @@ for ip = 1:length(plotorder)
     figure(2)
     subplot(length(plotorder),1,ip)
     set(gca,'ColorOrder',cc,'NextPlot','replacechildren');
-    semilogx(f',coh_stack(:,:,ip),'-','LineWidth',.5);%,'Color',cc);
+    semilogx(f',coh_stack(:,:,po_id(ip)),'-','LineWidth',.5); %,('Color',cc);
     hold on
     title(sprintf('%s Coherence: %s',station, plotorder{ip}))
     xlabel('Frequency (Hz)')
@@ -63,7 +73,7 @@ for ip = 1:length(plotorder)
     figure(3)
     subplot(length(plotorder),1,ip)
     set(gca,'ColorOrder',cc,'NextPlot','replacechildren');
-    semilogx(f',ph_stack(:,:,ip),'o','MarkerSize',1);
+    semilogx(f',ph_stack(:,:,po_id(ip)),'o','MarkerSize',1);
     hold on
     title(sprintf('%s Phase: %s',station,plotorder{ip}))
     xlabel('Frequency (Hz)')
@@ -75,7 +85,7 @@ for ip = 1:length(plotorder)
     figure(4)
     subplot(length(plotorder),1,ip)
     set(gca,'ColorOrder',cc,'NextPlot','replacechildren');
-    loglog(f',ad_stack(:,:,ip),'-','LineWidth',.5)
+    loglog(f',ad_stack(:,:,po_id(ip)),'-','LineWidth',.5)
     hold on
     title(sprintf('%s Admittance: %s',station,plotorder{ip}))
     xlabel('Frequency (Hz)')
